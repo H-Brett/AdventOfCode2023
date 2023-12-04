e098ecc851997e7baa8f67a23639460e29250cd6
Brett <horst.brett@gmail.com>
2023-12-04 14:28:49 -0700

    Day 4 Part 2, added git ignore for input files per awareness of AoC owner request

--- Day4/Javascript/Day4.js
+++ Day4/Javascript/Day4.js
@@ -1,5 +1,5 @@
 // H-Brett
-// 12.2.2023
+// 12.4.2023
 // Advent Calendar of Code 2023 Day 4
 
 const fs = require('fs')
@@ -15,34 +15,62 @@ fs.readFile('../input.txt', (err, data) => {
 		let processed = array.map((game) => {
 			let construct = {}
 			game = game.split(' | ')
-			construct['id'] = game[0].split(': ')[0].match(/\d{1,}/)[0]
+			construct['id'] = parseInt(game[0].split(': ')[0].match(/\d{1,}/)[0])
 			construct['card'] = game[0].split(': ')[1].split(/\s{1,}/)
 			construct['winnings'] = game[1].split(/\s{1,}/)
+			construct['timesPlayed'] = 0
+			construct['winCnt'] = 0
+			construct['singleWinSum'] = 0
 
 			return construct
 		})
-
 		return processed
 	}
 
 	let checkWinnings = (game) => {
+		let winTotal = 0
 		let winCnt = 0
-		console.log(game)
+		game.timesPlayed += 1
+
 		game.winnings.forEach((winVal) => {
 			game.card.forEach((checkVal) => {
 				 if(parseInt(winVal) == parseInt(checkVal)) {
-				 	console.log(winCnt)
-				 	winCnt == 0 ? winCnt += 1 : winCnt += winCnt
+				 	winCnt += 1
+				 	winTotal == 0 ? winTotal += 1 : winTotal += winTotal
 				 }
 			})
 		})
 
-		return winCnt
+		return [winCnt, winTotal]
+	}
+
+	let getTotals = (array) => {
+		let winSum = 0
+		let cardSum = 0
+
+		array.forEach((game, i, arr) => {
+			if(game.winCnt > 0) {
+				for(k = game.timesPlayed; k > 0; k--){
+					for(j = game.winCnt; j > 0; j--) {
+						arr[i+j].timesPlayed += 1
+					}	
+				}
+			}
+			winSum += game.singleWinSum
+			cardSum += game.timesPlayed
+		})
+
+		return [winSum, cardSum]
 	}
 
 	let procData = dataProc(inArr)
-	let winSum = 0
-	procData.forEach((game) => { winSum += checkWinnings(game) })
-	console.log(winSum)
-
+	procData.forEach((game, i, arr) => {
+		let checkTuple = checkWinnings(game)
+		game.winCnt += checkTuple[0]
+		game.singleWinSum += checkTuple[1]
+	})
+	
+	let answers = getTotals(procData)
+	console.log('Day 4 Part 1 answer:', answers[0])
+	console.log('Day 4 Part 2 answer:', answers[1])
 })
