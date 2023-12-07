8ee1e94800eeb810a1b606b6128408f6781265dd
Brett <horst.brett@gmail.com>
2023-12-06 17:08:21 -0700

    Day 6 Part 2, needs refactoring

--- Day6/Javascript/Day6.js
+++ Day6/Javascript/Day6.js
@@ -11,11 +11,34 @@ fs.readFile('../input.txt', (err, data) => {
 		return console.error(err);
 	}
 
-	const inArr = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().split(/\s+/) }) })
-	let races = []
-	for (i = 0; i < inArr[0][1].length; i++) {
-		races.push([parseInt(inArr[0][1][i]), parseInt(inArr[1][1][i])])
+	const part1Data = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().split(/\s+/) }) })
+	const part2Data = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().replace(/\s+/g, '') }) })
+
+	console.log(part2Data)
+
+	let part2Soln = (data) => {
+		let winCount = 0
+
+		console.log(data, data[0], data[1])
+
+		for(i = 1; i < data[0][1]; i++) {
+			let distance = (data[0][1] - i) * i
+			if (distance > data[1][1]) {
+				winCount += 1
+			}
+		}
+
+		console.log(winCount)
+		//possWinCount.push(winCount)
+
 	}
+
+	let races = []
+	for (i = 0; i < part1Data[0][1].length; i++) {
+		races.push([parseInt(part1Data[0][1][i]), parseInt(part1Data[1][1][i])])
+	}
+
+	// console.log(races)
 
 	let possWinCount = []
 	races.forEach((timeDistPair) => {
@@ -39,4 +62,5 @@ fs.readFile('../input.txt', (err, data) => {
 	}
 
 	console.log(multArr(possWinCount))
+	part2Soln(part2Data)
 })
