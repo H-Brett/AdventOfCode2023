a6d3447f5c1b1140eeddf30053b232585c4a9d82
Brett <horst.brett@gmail.com>
2023-12-06 19:12:23 -0700

    refactored Day 6

--- Day6/Javascript/Day6.js
+++ Day6/Javascript/Day6.js
@@ -2,7 +2,7 @@
 // 12.6.2023
 // Advent Calendar of Code Day 6
 // Part 1 1159152
-// Part 2
+// Part 2 41513103
 
 const fs = require("fs")
 
@@ -11,56 +11,41 @@ fs.readFile('../input.txt', (err, data) => {
 		return console.error(err);
 	}
 
-	const part1Data = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().split(/\s+/) }) })
+	let part1Data = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().split(/\s+/) }) })
+		part1Data[0] = part1Data[0][1] 
+		part1Data.splice(1, 1, part1Data[1][1])
+		part1Data.unshift([])
+		for (i = 0; i < part1Data[1].length; i++) {
+			part1Data[0].push([parseInt(part1Data[1][i]), parseInt(part1Data[2][i])])
+		}
+		part1Data.pop(); part1Data.pop();
+		part1Data = part1Data[0]
+
 	const part2Data = data.toString().split('\r\n').map((line) => { return line.split(':').map((split) => { return split.trim().replace(/\s+/g, '') }) })
+		part2Data[0] = part2Data[0][1] 
+		part2Data.splice(1, 1, part2Data[1][1])
 
-	console.log(part2Data)
-
-	let part2Soln = (data) => {
+	let calcWins = (timeDistPair) => {
 		let winCount = 0
-
-		console.log(data, data[0], data[1])
-
-		for(i = 1; i < data[0][1]; i++) {
-			let distance = (data[0][1] - i) * i
-			if (distance > data[1][1]) {
+		let [ time, dist ] = timeDistPair
+			
+		for(i = 1; i < time; i++) {
+			let distance = (time - i) * i
+			if (distance > dist) {
 				winCount += 1
 			}
 		}
 
-		console.log(winCount)
-		//possWinCount.push(winCount)
-
+		return winCount
 	}
 
-	let races = []
-	for (i = 0; i < part1Data[0][1].length; i++) {
-		races.push([parseInt(part1Data[0][1][i]), parseInt(part1Data[1][1][i])])
-	}
+	let part1Answer = 1
+		part1Data.forEach((race) => {
+			part1Answer *= calcWins(race)
+		})
 
-	// console.log(races)
-
-	let possWinCount = []
-	races.forEach((timeDistPair) => {
-		let winCount = 0
-
-		for(i = 1; i < timeDistPair[0]; i++) {
-			let distance = (timeDistPair[0] - i) * i
-			if (distance > timeDistPair[1]) {
-				winCount += 1
-			}
-		}
-
-		possWinCount.push(winCount)
-	})
-
-
-	let multArr = (array) => {
-		return array.reduce((accumulator, currentValue) => {
-			return accumulator * currentValue
-		},1)
-	}
-
-	console.log(multArr(possWinCount))
-	part2Soln(part2Data)
+	console.time()
+	console.log('Day 6 Part 1 answer:', part1Answer)
+	console.log('Day 6 Part 2 answer:', calcWins(part2Data))
+	console.timeEnd() // 6 seconds both solutions
 })
