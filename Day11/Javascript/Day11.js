9ecbaa4412fe576edd8d286ea00431d405cc5377
Brett <horst.brett@gmail.com>
2023-12-11 17:30:56 -0700

    Day 11 Part 2, better algorithm calculates new galaxy offset instead of looping through entire array for galaxy indexes

--- Day11/Javascript/Day11.js
+++ Day11/Javascript/Day11.js
@@ -2,7 +2,7 @@
 // 12.11.2023
 // Advent Calendar of Code Day 11
 // Part 1 10313550
-// Part 2 82,000,210 too low
+// Part 2 611998089572
 
 const fs = require("fs")
 
@@ -30,11 +30,21 @@ fs.readFile('../input.txt', (err, data) => {
 				transposedInput[j].push(line[j])
 			}
 		}
-		
-		if(!/\#/.test(line)) {
+
+		if(/\#/.test(line)) {
+			while(/\#/.test(line)) {
+				let match = line.match(/\#/)
+
+				galaxyIndex.push([i, match.index])
+
+				line = line.replace(/\#/, '.')
+				//galaxyCount++
+			}
+		} else {
 			emptyRow.push(i)
-		} 
+		}
 	})
+
 
 	transposedInput.forEach((line, i) => {
 		line = line.join('')
@@ -45,97 +55,30 @@ fs.readFile('../input.txt', (err, data) => {
 		}
 	})
 
-	console.log(emptyCol)
-	console.log(emptyRow)
-
-	//inArr.forEach((line) => console.log(line))
-	// console.log(inArr[0].length)
-
-
-	inArr.forEach((line) => {
-		//console.log(line)
-		let expLine = line.split('')
-		//console.log(expLine)
-
-		for(let i = 0; i < emptyCol.length; i++) {
-			let insert = Array(999999).fill('.')
-			expLine.splice(emptyCol[i] + i, 0, insert.join(''))
-		}
-
-		expanded.push(expLine.join(''))
-	})
-
-	console.log(expanded[0].length)
-
-	for(let i = 0; i < emptyRow.length; i++) {
-		
-		//console.log(emptyRow[i])
-
-		//let top = expanded.slice(0, emptyRow[i])
-		let emptyLine = '.'.repeat(expanded[0].length)
-		let insert = Array(999999).fill(emptyLine)
-		expanded.splice(emptyRow[i] + i, 0, insert)
-		//console.log(expanded)
-		
-		/*
-		for(j=0; j < 10; j++) {
-			expanded.splice(emptyRow[i] + j, 0, '.'.repeat(expanded[0].length))
-		}
-		*/
-	}
-
-	expanded = expanded.flat()
-
-	console.log(expanded.length)
-
-	expanded.forEach((line, i) => {
-		if(/\#/.test(line)) {
-			while(/\#/.test(line)) {
-				let match = line.match(/\#/)
-				galaxyIndex.push([i, match.index])
-				line = line.replace(/\#/, '.')
+	galaxyIndex.forEach((galaxy) => {
+		for(i = emptyCol.length; i >= 0; i--) {
+			if(galaxy[1] > emptyCol[i]) {
+				galaxy[1] += 999999
 			}
 		}
 
+		for(i = emptyRow.length; i >= 0 ; i--) {
+			if(galaxy[0] > emptyRow[i]) {
+				galaxy[0] += 999999
+			}
+		}
 	})
 
-	//console.log(expanded)
-	//console.log(galaxyIndex)
+
 
 	let distanceSum = 0
-
 	galaxyIndex.forEach((galaxy, i) => {
-		//take in first galaxy
 
 		for(j = i + 1; j < galaxyIndex.length; j++) {
-
 			distanceSum += Math.abs((galaxyIndex[j][0] - galaxy[0])) + Math.abs((galaxyIndex[j][1] - galaxy[1]))
-			//console.log(distanceSum) 
 		}
 	})
 
 	console.log(distanceSum)
-
-
-
-
-	//console.log(galaxyIndex.length)
-	//console.log(galaxyCount)
-	//console.log(inArr)
 });
 
-/*
-if(/\#/.test(line)) {
-			while(/\#/.test(line)) {
-				let match = line.match(/\#/)
-
-				galaxyIndex.push([i, match.index])
-
-				line = line.replace(/\#/, galaxyCount)
-				galaxyCount++
-			}
-		} else {
-			emptyRow.push(i)
-		}
-
-*/ 
